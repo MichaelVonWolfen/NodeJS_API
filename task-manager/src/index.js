a022211516cfc4105bf68e983230a4e06429106b
mihai.stoica <mihai.stoica@bearingpoint.com>
2021-05-31 15:08:13 +0300

    Added routes in src folder

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,17 +1,13 @@
 const express = require('express')
-const Users = require('./models/user')
-require('./db/mongoose')
-const Tasks = require('./models/task')
-const { ObjectID } = require('bson')
-const { update } = require('./models/user')
-
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.port || 3000
 
 app.use(express.json())
-
-
+app.use(userRouter)
+app.use(taskRouter)
 
 app.listen(port, ()=>{
     console.log(`Running on port ${port}`)
