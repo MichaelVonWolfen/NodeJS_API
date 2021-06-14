072c1c960f71915b63f439322e2ffdd5dbe8d618
Mihai Stoica <41210245+MichaelVonWolfen@users.noreply.github.com>
2021-06-14 13:44:29 +0300

    Small changes. IDK what they are

--- notes_app/app.js
+++ notes_app/app.js
@@ -1,61 +1,61 @@
-const { command, string } = require('yargs');
-const yargs = require('yargs');
-const notes = require("./notes")
-yargs.version('2.0.2')
+const { command, string } = require("yargs");
+const yargs = require("yargs");
+const notes = require("./notes");
+yargs.version("2.0.2");
 
 yargs.command({
-    command:"add",
-    description:"Adds a note",
-    builder:{
-        title:{
-            describe:"Note Title",
-            demandOption:true,
-            type:String
-        },
-        body:{
-            describe:"Note body",
-            demandOption:true,
-            type:String
-        }
+  command: "add",
+  description: "Adds a note",
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: String,
     },
-    handler(argv){
-        notes.addNotes(argv.title, argv.body)
-    }
-})
-yargs.command({
-    command:"rmv",
-    description:"Removes a note",
-    builder:{
-        title:{
-            describe:"Note Title",
-            demandOption:true,
-            type:String
-        }
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: String,
     },
-    handler(argv){
-        notes.remove(argv.title)
-    }
-})
+  },
+  handler(argv) {
+    notes.addNotes(argv.title, argv.body);
+  },
+});
 yargs.command({
-    command:"list",
-    description:"Lists all notes",
-    
-    handler(argv){
-        notes.listNotes()
-    }
-})
-yargs.command({
-    command:"read",
-    description:"Finds a note and prints it's content",
-    builder:{
-        title:{
-            describe:"Note to search for",
-            demandOption:true,
-            type:String
-        },
+  command: "rmv",
+  description: "Removes a note",
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: String,
     },
-    handler(argv){
-        notes.read(argv.title)
-    }
-})
-yargs.parse()
+  },
+  handler(argv) {
+    notes.remove(argv.title);
+  },
+});
+yargs.command({
+  command: "list",
+  description: "Lists all notes",
+
+  handler(argv) {
+    notes.listNotes();
+  },
+});
+yargs.command({
+  command: "read",
+  description: "Finds a note and prints it's content",
+  builder: {
+    title: {
+      describe: "Note to search for",
+      demandOption: true,
+      type: String,
+    },
+  },
+  handler(argv) {
+    notes.read(argv.title);
+  },
+});
+yargs.parse();
--- notes_app/notes.js
+++ notes_app/notes.js
@@ -1,71 +1,72 @@
-const fs = require("fs")
-const { join } = require("path")
-const chalk = require("chalk")
-const get_Notes = function(){
-
-}
-const add_notes =(title, body)=>{
-    const notes = load_Notes()
-    // const duplicate = notes.filter(function(note){
-    //     return note.title === title
-    // })
-    debugger
-    const duplicate = notes.find((note) => note.title === title)
-    if(!duplicate){
-        notes.push({
-            title: title,
-            body: body
-        })
-        saveNotes(notes)
-        console.log(chalk.green.inverse("Note title added"))
-    }else{
-        console.log(chalk.yellow.inverse("Note title taken!"))
-    }
-}
-const load_Notes = ()=>{
-    try{
-        const data_buff = fs.readFileSync("notes.json")
-        const data_JSON = data_buff.toString()
-        return JSON.parse(data_JSON)
-    }catch(e){
-        return []
-    }
-}
-const remove_note = (title)=>{
-    const notes = load_Notes()
-    let notes_to_keep = notes.filter((note)=> title !== note.title)
-    if(notes_to_keep.length !== notes.length)
-        console.log(chalk.green.inverse(`Note "${title}" removed!`))
-    else
-        console.log(chalk.red.inverse(`Note ${title} does not exists!\n\nNothing was changed!`))
-        saveNotes(notes_to_keep)
-}
-const saveNotes = (notes) =>{
-    fs.writeFileSync("notes.JSON", JSON.stringify(notes));
-};
-const list_notes = ()=>{
-    const notes = load_Notes()
-    console.log(chalk.green("Your Notes:"))
-    notes.forEach(note => {
-        console.log(`${chalk.red('@')}\t${note.title}`)
+const fs = require("fs");
+const { join } = require("path");
+const chalk = require("chalk");
+const get_Notes = function () {};
+const add_notes = (title, body) => {
+  const notes = load_Notes();
+  // const duplicate = notes.filter(function(note){
+  //     return note.title === title
+  // })
+  debugger;
+  const duplicate = notes.find((note) => note.title === title);
+  if (!duplicate) {
+    notes.push({
+      title: title,
+      body: body,
     });
-}
-const read_note = (title)=>{
-    const notes = load_Notes()
-    const searched_note = notes.find((note) => note.title === title)
+    saveNotes(notes);
+    console.log(chalk.green.inverse("Note title added"));
+  } else {
+    console.log(chalk.yellow.inverse("Note title taken!"));
+  }
+};
+const load_Notes = () => {
+  try {
+    const data_buff = fs.readFileSync("notes.json");
+    const data_JSON = data_buff.toString();
+    return JSON.parse(data_JSON);
+  } catch (e) {
+    return [];
+  }
+};
+const remove_note = (title) => {
+  const notes = load_Notes();
+  let notes_to_keep = notes.filter((note) => title !== note.title);
+  if (notes_to_keep.length !== notes.length)
+    console.log(chalk.green.inverse(`Note "${title}" removed!`));
+  else
+    console.log(
+      chalk.red.inverse(
+        `Note ${title} does not exists!\n\nNothing was changed!`
+      )
+    );
+  saveNotes(notes_to_keep);
+};
+const saveNotes = (notes) => {
+  fs.writeFileSync("notes.JSON", JSON.stringify(notes));
+};
+const list_notes = () => {
+  const notes = load_Notes();
+  console.log(chalk.green("Your Notes:"));
+  notes.forEach((note) => {
+    console.log(`${chalk.red("@")}\t${note.title}`);
+  });
+};
+const read_note = (title) => {
+  const notes = load_Notes();
+  const searched_note = notes.find((note) => note.title === title);
 
-    if(searched_note){
-        console.log(chalk.green(title))
-        console.log(searched_note.body)
-    }else{
-        console.log(chalk.red.greenBG.inverse(`"${title}" was not found!`))
-    }
-}
+  if (searched_note) {
+    console.log(chalk.green(title));
+    console.log(searched_note.body);
+  } else {
+    console.log(chalk.red.greenBG.inverse(`"${title}" was not found!`));
+  }
+};
 module.exports = {
-    getNotes: get_Notes, 
-    addNotes: add_notes,
-    remove:remove_note,
-    listNotes:list_notes,
-    read:read_note
-
-}
+  getNotes: get_Notes,
+  addNotes: add_notes,
+  remove: remove_note,
+  listNotes: list_notes,
+  read: read_note,
+};
