c0db5a18f714d2652d79fa3f205c22788c80372e
Mihai Stoica <41210245+MichaelVonWolfen@users.noreply.github.com>
2021-06-17 00:49:38 +0300

    Application can receive messages from other users and show them on screen

--- chat_app/public/js/chat.js
+++ chat_app/public/js/chat.js
@@ -1,11 +1,62 @@
 var socket = io();
 
-socket.on("countUpdated", (msg) => {
-	let h = document.getElementById("messsage");
-	h.innerText = msg;
-	console.log("The counter has been updated:", msg);
+socket.on("general_msg", (msg) => {
+	console.log(msg);
 });
-document.querySelector("#increment").addEventListener("click", () => {
-	console.log("Clicked!");
-	socket.emit("incrementCounter");
+const $messageForm = document.querySelector("#message_form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $locationButton = document.querySelector("#send_location");
+const $messages = document.querySelector("#messages");
+
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationTemplate = document.querySelector("#location-template").innerHTML;
+
+document.addEventListener("DOMContentLoaded", () => {
+	$messageForm.addEventListener("submit", (e) => {
+		e.preventDefault();
+		$messageFormButton.setAttribute("disabled", "disabled");
+		let message = e.target.elements.message_field.value;
+		socket.emit("message", message, (error) => {
+			$messageFormButton.removeAttribute("disabled");
+			if (error) return console.log(error);
+			else console.log("Message sent succesfully!");
+			$messageFormInput.value = "";
+		});
+		// message_field.value = "";
+	});
+});
+socket.on("message", (msg) => {
+	let html = Mustache.render(messageTemplate, {
+		message: msg.text,
+		CreatedAt: moment(msg.createdAt).format("HH:mm"),
+	});
+	$messages.insertAdjacentHTML("beforeend", html);
+});
+socket.on("rcvLocation", (msg) => {
+	let html = Mustache.render(locationTemplate, {
+		url: msg.url,
+		CreatedAt: moment(msg.createdAt).format("HH:mm"),
+	});
+	$messages.insertAdjacentHTML("beforeend", html);
+});
+$locationButton.addEventListener("click", () => {
+	if (!navigator.geolocation) {
+		return alert("Your browser does not dupport geolocation");
+	}
+	$locationButton.setAttribute("disabled", "disabled");
+	navigator.geolocation.getCurrentPosition((position) => {
+		socket.emit(
+			"sendLocation",
+			{
+				latitude: position.coords.latitude,
+				longitude: position.coords.longitude,
+			},
+			(error) => {
+				$locationButton.removeAttribute("disabled");
+				if (error) return console.log(error);
+				else console.log("Location shared succesfully!");
+			}
+		);
+	});
 });
--- chat_app/src/index.js
+++ chat_app/src/index.js
@@ -1,13 +1,28 @@
 const { app, server, io } = require("./app");
 const PORT = process.env.PORT || 3000;
-
-var count = 0;
+const {
+	generateMessage,
+	generateLocationMessage,
+} = require("./utils/messages");
 io.on("connection", (socket) => {
 	console.log("New Websocket connection!");
-	socket.emit("countUpdated", count);
-	socket.on("incrementCounter", () => {
-		count++;
-		io.emit("countUpdated", count);
+	// socket.emit("countUpdated", count);
+	// socket.on("incrementCounter", () => {
+	// 	count++;
+	// 	io.emit("countUpdated", count);
+	// });
+	socket.emit("message", generateMessage("Welcome!"));
+	socket.broadcast.emit("message", generateMessage("A new user has joined!"));
+	socket.on("message", (msg, callback) => {
+		io.emit("message", generateMessage(msg));
+		callback();
+	});
+	socket.on("sendLocation", ({ latitude, longitude }, callback) => {
+		io.emit("rcvLocation", generateLocationMessage(latitude, longitude));
+		callback();
+	});
+	socket.on("disconnect", () => {
+		io.emit("general_msg", generateMessage("An user has left!"));
 	});
 });
 server.listen(PORT, () => {
