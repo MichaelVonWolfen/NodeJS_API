13f150949eb3446f6f0e19b0c3de17bcf8bc2f79
Mihai Stoica <41210245+MichaelVonWolfen@users.noreply.github.com>
2021-07-08 22:32:38 +0300

    .

--- RecepyProj/src/app/recepies/recepie-list/recepie-list.component.ts
+++ RecepyProj/src/app/recepies/recepie-list/recepie-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recepie-list',
@@ -6,7 +7,10 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./recepie-list.component.css']
 })
 export class RecepieListComponent implements OnInit {
-
+  recipes:Recipe[] = [
+    new Recipe('Test recipe', "This is a test recipe!", 'https://retete.unica.ro/wp-content/uploads/2013/10/lasagna.jpg'),
+    new Recipe('Test recipe', "This is a test recipe!", 'https://retete.unica.ro/wp-content/uploads/2013/10/lasagna.jpg'),
+  ]
   constructor() {
   }
 
